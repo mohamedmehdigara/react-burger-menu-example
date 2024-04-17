323eba3137d9bd576c0dfff7339a87c165753d53
mohamedmehdigara <garamehdi57@yahoo.com>
2024-04-17 19:12:22 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import Sidebar from './components/Sidebar';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ContentCards from './components/ContentCards';
+import Sidebar from './components/Sidebar/Sidebar';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import ContentCards from './components/ContentCards/ContentCards';
 import './App.css';
 
 function App() {
